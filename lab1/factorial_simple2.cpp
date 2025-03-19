d079c7ec29263111116f30262a098e521ec349af
Olga Nikolaevna <144958680+ONDubovickaya@users.noreply.github.com>
2025-03-20 00:36:11 +0300

    Update factorial_simple2.cpp

--- lab1/factorial_simple2.cpp
+++ lab1/factorial_simple2.cpp
@@ -4,11 +4,11 @@
 
 int factorial(int n) {
     if (n <= 1) {
-        return 1; // базовый случай: 0! и 1! равны 1
+        return 1;  // базовый случай: 0! и 1! равны 1
         
     } else {
-        int result = factorial(n - 1); // рекурсивный вызов
-        return n * result; // возвращаем результат
+        int result = factorial(n - 1);  // рекурсивный вызов
+        return n * result;  // возвращаем р-т вычисления факториала
     }
 }
 
